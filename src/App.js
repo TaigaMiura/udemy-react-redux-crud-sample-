cdeba9079a5ff3ff7514f408f8c1a74d06bdcafe
TaigaMiura <taigamiura22@gmail.com>
2019-11-05 20:42:32 +0900

    learning component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,16 @@
-import React, { Component } from 'react';
+// import React, { Component } from 'react';
+import React from 'react';
 
-class App extends Component {
-  render() {
-    // return <h1>Hello, world</h1>
-    return (
-      <>
-        <label htmlFor="for">bar</label>
-        <input type="text" onClick={() => { console.log('hi') }} />
-      </>
-    )
-  }
-}
+// class App extends Component {
+//   render() {
+//     // return <h1>Hello, world</h1>
+//     return (
+//       <>
+//         <label htmlFor="for">bar</label>
+//         <input type="text" onClick={() => { console.log('hi') }} />
+//       </>
+//     )
+//   }
+// }
+const App = () => <div>Hi</div>
 export default App;
