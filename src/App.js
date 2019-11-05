c0c8c93456c886ad0e06d0b41af3d87cbdc2134f
TaigaMiura <taigamiura22@gmail.com>
2019-11-05 20:29:40 +0900

    jsxの文法について学習

--- src/App.js
+++ src/App.js
@@ -2,7 +2,13 @@ import React, { Component } from 'react';
 
 class App extends Component {
   render() {
-    return <h1>Hello, world</h1>
+    // return <h1>Hello, world</h1>
+    return (
+      <>
+        <label htmlFor="for">bar</label>
+        <input type="text" onClick={() => { console.log('hi') }} />
+      </>
+    )
   }
 }
 export default App;
