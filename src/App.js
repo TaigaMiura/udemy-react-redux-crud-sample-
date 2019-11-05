06305560654fd1ce7f8c7782989ef1d3499c9889
TaigaMiura <taigamiura22@gmail.com>
2019-11-05 21:00:06 +0900

    props basic 3 default props

--- src/App.js
+++ src/App.js
@@ -3,25 +3,28 @@ import React from 'react';
 const App = () => {
   const profiles = [
     { name: "taro", age: 10 },
-    { name: "ttt", age: 10000 }
+    { name: "ttt", age: 10000 },
+    { name: "ttttttttttttt" }
   ]
   return (
     <div>
       {
         profiles.map((profile, index) => {
-          return <User name={profile.name} age={profile.age} key={index}/>
+          return (
+            <User
+              name={profile.name}
+              age={profile.age}
+              key={index}
+            />
+          )
         })
-    }
-{/*
-
-index.js:1375 Warning: Each child in a list should have a unique "key" prop.
-
-Check the render method of `App`. See https://fb.me/react-warning-keys for more information.
-in User (at App.js:12)
-in App (at src/index.js:7)
-*/}
+      }
     </div>
   )
 }
 const User = props => <div>Hi, I am {props.name}, and {props.age} age</div>
+User.defaultProps = {
+  age: 1
+}
+
 export default App;
